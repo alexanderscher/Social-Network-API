f968919909366f10a72b0a58a302365d412d395b
alexanderscher <alexcscher@gmail.com>
2023-05-03 18:54:10 -0700

    user model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: String,
+    email: String,
+    age: Number,
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+userSchema.virtual("friendCount").get(function () {
+  return this.friend.length;
+});
+
+// Initialize our User model
+const User = model("user", userSchema);
+
+module.exports = User;
